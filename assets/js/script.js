de2fe664e901d17f7e1f7f99934be884f97e66a8
Stjepan Mesaros <stjepan.mesaros@gmail.com>
2023-09-20 00:20:50 +0100

    ADD: ability to choose type of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 let bartender = document.getElementsByTagName("img");
-let buttons = document.getElementsByTagName("button")
-let buttonsCss = document.querySelector(".button")
+let buttons = document.getElementsByClassName("button");
+let header = document.getElementsByTagName("h1");
+let paragraph = document.getElementsByTagName("p");
+let div = document.getElementsByTagName("div");
 
-setTimeout(()=> {bartender[0].src = "assets/images/bartender3.png"},3000);
+
+
+
+
+setTimeout(()=> {bartender[0].src = "assets/images/bartender3.png"},1000);
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", chosenNumberOfDice);
@@ -14,5 +20,30 @@ function chosenNumberOfDice () {
     buttons[0].innerHTML = "Single Game";
     buttons[1].innerHTML = "Against a PC"
     buttons[2].innerHTML = "Against a 2nd Player"
-    //buttonsCss.style.margin = "-25px auto 10px auto";
+    
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", gameModeSelected);
+    }
 }
+
+function  gameModeSelected (event) {
+    let gameModePressed = event.value;
+    header[0].style.display = "none";
+    paragraph[0].style.display = "none";
+    bartender[0].src = "assets/images/bartender5.png";
+    
+    setTimeout(()=> {
+        bartender[0].src = "assets/images/bartender.png";
+    },1000);
+    bartender[0].style.maxWidth = "80%";
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.display = "flex";
+        buttons[i].style.justifyContent = "space-between";
+        buttons[i].style.width = "30%";
+    }
+    buttons[2].style.display = "none";
+    div[0].style.display = "flex"
+    div[0].style.flexDirection = "row";
+
+}
+
