c708233f727ddae9127b9f830d6d679ff0a30822
Stjepan Mesaros <stjepan.mesaros@gmail.com>
2023-09-20 09:58:07 +0100

    ADD: Dice and table

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
-let bartender = document.getElementsByTagName("img");
+let bartender = document.getElementById("bartender-image");
 let buttons = document.getElementsByClassName("button");
 let header = document.getElementsByTagName("h1");
 let paragraph = document.getElementsByTagName("p");
 let div = document.getElementsByTagName("div");
+let table = document.getElementById("table-image")
+let dice = document.getElementsByClassName("dice-images")
 
 
 
-
-setTimeout(()=> {bartender[0].src = "assets/images/bartender3.png"},1000);
+setTimeout(()=> {bartender.src = "assets/images/bartender3.png"},1000);
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", chosenNumberOfDice);
 }
 
 function chosenNumberOfDice () {
-    bartender[0].src = "assets/images/bartender4.png";
+    bartender.src = "assets/images/bartender4.png";
     buttons[2].style.display = "flex";
     buttons[0].innerHTML = "Single Game";
     buttons[1].innerHTML = "Against a PC"
@@ -29,12 +30,12 @@ function  gameModeSelected (event) {
     //let gameModePressed = event.value;
     header[0].style.display = "none";
     paragraph[0].style.display = "none";
-    bartender[0].src = "assets/images/bartender5.png";
+    bartender.src = "assets/images/bartender5.png";
     
     setTimeout(()=> {
-        bartender[0].src = "assets/images/bartender.png";
+        bartender.src = "assets/images/bartender.png";
     },1000);
-    bartender[0].style.maxWidth = "80%";
+    bartender.style.maxWidth = "80%";
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.display = "flex";
         buttons[i].style.justifyContent = "space-between";
@@ -43,5 +44,12 @@ function  gameModeSelected (event) {
     buttons[2].style.display = "none";
     div[0].style.display = "flex"
     div[0].style.flexDirection = "row";
+    
+}
+
+function showTableAndDice() {
+    table.style.display = "flex"
+    dice[0].style.display = "flex"
+    
 }
 
